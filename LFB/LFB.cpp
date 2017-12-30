44a421db6caf1ecc2141d17e0971d2629061cb12
Mojtaba <mjafaari99@gmail.com>
2017-12-30 16:39:11 +0330

    some bugs fixed

--- LFB/LFB.cpp
+++ LFB/LFB.cpp
@@ -8,17 +8,17 @@ void init(){
 
 int next(int previous_opponent_move){
 	if(previous_opponent_move == 0)
-		if(rand() / RAND_MAX < 2.0 / 3)
+		if(rand() * 1.0 / RAND_MAX < 2.0 / 3)
 			return 2;
 		else
 			return 0;
 	if(previous_opponent_move == 1)
-		if(rand() / RAND_MAX < 2.0 / 3)
+		if(rand() * 1.0 / RAND_MAX < 2.0 / 3)
 			return 0;
 		else
 			return 1;
 	if(previous_opponent_move == 2)
-		if(rand() / RAND_MAX < 2.0 / 3)
+		if(rand() * 1.0 / RAND_MAX < 2.0 / 3)
 			return 1;
 		else
 			return 2;
