84a02ee77493050ed7b995d2bbd0ea09ae8bc44b
Mojtaba <mjafaari99@gmail.com>
2017-12-30 12:12:48 +0330

    version 1.01

---
+++ LFB/LFB.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include "riazat.h"
+#include <time.h>
+
+void init(){
+	srand(time(0));
+}
+
+int next(int previous_opponent_move){
+	if(previous_opponent_move == 0)
+		if(rand() / RAND_MAX < 2.0 / 3)
+			return 2;
+		else
+			return 0;
+	if(previous_opponent_move == 1)
+		if(rand() / RAND_MAX < 2.0 / 3)
+			return 0;
+		else
+			return 1;
+	if(previous_opponent_move == 2)
+		if(rand() / RAND_MAX < 2.0 / 3)
+			return 1;
+		else
+			return 2;
+	return rand() % 3;
+}
