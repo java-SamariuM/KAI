522d72245e298f79115412f2751247bdec016ac2
SoroushVT <svafaiet@gmail.com>
2018-01-01 18:51:25 +0330

    recieving image and picture was saved in a black and white mode in BW array but recieve image has problem

---
+++ StarCounter/StarCounter.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int R[810][810];
+int G[810][810];
+int B[810][810];
+int BW[810][810];
+int w, h , expected;
+int range = 10;
+
+void recieveImage();
+
+void makeBW();
+
+//double change(int x1, int y1, int x2, int y2);
+
+#define Debug
+
+int main(){
+    recieveImage();
+    //makeBW();
+    #ifdef Debug
+    for(int i = 0; i < h; i++){ //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee  make < <=
+        for(int j = 0; j < w; j++){
+            printf("(%d, %d, %d) ", R[i][j], G[i][j], B[i][j]);
+        }
+    }
+    #endif // Debug
+
+}
+
+void recieveImage(){
+    cin >>  h >> w >> expected;
+    for(int i = 0; i < h; i++){  //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee make < <=
+        for(int j = 0; j < w; j++){
+            scanf("(%d, %d, %d) ", &R[i][j], &G[i][j], &B[i][j]);
+        }
+        scanf("\n");
+    }
+}
+
+void makeBW(){
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            double ans = R[i][j] * 0.3;
+            ans += G[i][j] * 0.59;
+            ans += B[i][j] * 0.21;
+            BW[i][j] = ans;
+        }
+    }
+}
+/*
+double change(int x1, int y1, int x2, int y2){
+     int dR = R[x1] - R[x2];
+     int dG = G[x1] - G[x2];
+     int dB = B[x1] - B[x2];
+
+}*/
