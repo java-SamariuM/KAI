8aefd58ad2e03675b88ad21ecf0fd57cd1b65de5
SoroushVT <svafaiet@gmail.com>
2018-01-02 17:23:02 +0330

    blur and blurStar function is made and BW was changed to MAP, subMap was made for blurStar function

--- StarCounter/StarCounter.cpp
+++ StarCounter/StarCounter.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
+#define whiteEdge 220
+
 int R[810][810];
 int G[810][810];
 int B[810][810];
-int BW[810][810];
-int w, h , expected;
+int MAP[810][810];
+int subMAP[810][810];
+int star[50][3];
+int width, height , expected;
 int range = 10;
 
 void recieveImage();
 
 void makeBW();
+// make pic black and white
+
+void blurStar();
+
+int blur(int h, int w);
+
+int findStar();
+
 
 //double change(int x1, int y1, int x2, int y2);
 
@@ -20,10 +32,10 @@ void makeBW();
 
 int main(){
     recieveImage();
-    //makeBW();
+    makeBW();
     #ifdef Debug
-    for(int i = 0; i < h; i++){ //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee  make < <=
-        for(int j = 0; j < w; j++){
+    for(int i = 0; i < height; i++){ //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee  make < <=
+        for(int j = 0; j < width; j++){
             printf("(%d, %d, %d) ", R[i][j], G[i][j], B[i][j]);
         }
     }
@@ -32,26 +44,59 @@ int main(){
 }
 
 void recieveImage(){
-    cin >> h >> w >> expected;
+    cin >> height >> width >> expected;
     //scanf("\n");
-    for(int i = 0; i < h; i++){  //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee make < <=
-        for(int j = 0; j < w; j++){
+    for(int i = 0; i < height; i++){  //hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee make < <=
+        for(int j = 0; j < width; j++){
             scanf("%*[^0-9]%d%*[^0-9]%d%*[^0-9]%d", &R[i][j], &G[i][j], &B[i][j]);
         }
         //if(i != h-1)scanf("\n");
     }
+    return;
 }
 
 void makeBW(){
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < w; j++){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
             double ans = R[i][j] * 0.3;
             ans += G[i][j] * 0.59;
             ans += B[i][j] * 0.21;
-            BW[i][j] = ans;
+            MAP[i][j] = ans;
         }
     }
+    return;
 }
+
+void blurStar(){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            if((i == 0) || (j == 0) || (i == height - 1) || (j == width - 1)){
+                subMAP[i][j] = 0;
+            }
+            else{
+                subMAP[i][j] = blur(i, j);
+            }
+        }
+    }
+    memcpy(MAP, subMAP, 4*810*810); //maybe need change
+    return;
+}
+
+int blur(int h, int w){
+    double ans = 0;
+    for(int i = -1; i <= 1; i++){
+        for(int j = -1; j <= 1; j++){
+            ans += MAP[h + i][w + j];
+        }
+    }
+    ans /=9;
+    return ((int)ans);
+}
+
+int findStar(){
+
+}
+
 /*
 double change(int x1, int y1, int x2, int y2){
      int dR = R[x1] - R[x2];
